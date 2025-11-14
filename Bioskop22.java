c4428d0a2ba384a8bd55b2c050786dc05e6776f3
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-11-14 13:54:13 +0700

    Percobaan 1 Pertanyaan no.8

--- Bioskop22.java
+++ Bioskop22.java
@@ -22,10 +22,12 @@ public class Bioskop22 {
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
 
-       System.out.println();
-       System.out.println("Penonton pada baris ke-3: ");
-       for (int i = 0; i < penonton[2].length; i++) {
-            System.out.println(penonton[2][i]);
+        System.out.println();
+        System.out.println("Penonton pada baris ke-3");
+        for (String i  : penonton[2]) {
+        System.out.println(i);
+        
        }
+
     }
 }
