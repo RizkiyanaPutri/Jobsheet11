3ff0c15c4e82b2ef55680e644bf29f4cd8d60c66
RizkiyanaPutri <rizkiyanaputri8e30@gmail.com>
2025-11-19 21:19:14 +0700

    Percobaan 2 Pertanyaan no.2

--- BioskopWithScanner22.java
+++ BioskopWithScanner22.java
@@ -2,27 +2,45 @@ import java.util.Scanner;
 public class BioskopWithScanner22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int baris, kolom;
-        String nama, next;
         String[][] penonton = new String[4][2];
+        int menu = 0;
 
         while (true) {
-            System.out.print("Masukan nama  :");
-            nama = sc.nextLine();
-            System.out.print("Masukan baris :");
-            baris = sc.nextInt();
-            System.out.print("Masukan kolom :");
-            kolom = sc.nextInt();
+            System.out.println("\n=== MENU BIOSKOP ===");
+            System.out.println("1. Input data penonton");
+            System.out.println("2. Tampilkan daftar penonton");
+            System.out.println("3. Exit");
+            System.out.print("Pilih menu: ");
+            menu = sc.nextInt();
             sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            if (menu == 1) {
+                System.out.print("Masukkan nama       : ");
+                String nama = sc.nextLine();
+                System.out.print("Masukkan baris(1-4) : ");
+                int baris = sc.nextInt();
+                System.out.print("Masukkan kolom (1-2): ");
+                int kolom = sc.nextInt();
+                sc.nextLine();
+                penonton[baris - 1][kolom - 1] = nama;
+                System.out.println("Data penonton berhasil disimpan.");
 
-            System.out.print("Input penonton lainnya? (y/n): ");
-            next = sc.nextLine();
-
-            if (next.equalsIgnoreCase("n")){
+            }else if (menu == 2) {
+                System.out.println("\n=== DAFTAR PENONTON ===");
+                for (int i = 0; i < penonton.length; i++){
+                    for (int j = 0; j < penonton[i].length; j++) {
+                        String isi = (penonton[i][j] == null) ? "null" : penonton[i][j];
+                        System.out.print(isi + "\t");
+                    }
+                    System.out.println();
+                }
+            } else if(menu == 3){
+                System.out.println("Program selesai.");
                 break;
+            }else{
+                System.out.println("Menu tidak tersedia!");
             }
         }
+        sc.close();
     }
 }
